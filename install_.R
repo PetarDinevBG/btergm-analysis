ba5d1697673626d14f9197b78f2afba982d510b5
PetarDinevBG <pdinev@u.rochester.edu>
2020-04-26 14:54:14 +0200

    Update install_.R

--- install_.R
+++ install_.R
@@ -1,9 +1,3 @@
-install.packages("ggplot2")
-install.packages("dplyr")
-install.packages("data.table")
-install.packages("igraph")
-install.packages("scales")
-install.packages("lda")
 install.packages("btergm")
 
 
